99e9c9a0767c6af957baebe7c89938365f7bb6c4
luisangel <luispfcanales@gmail.com>
2021-08-25 16:15:51 -0500

    send

--- db.sql
+++ db.sql
@@ -2,10 +2,10 @@ create table cloud(
 user_id serial PRIMARY KEY,
 username VARCHAR(50) UNIQUE NOT NULL,
 email VARCHAR(50) UNIQUE NOT NULL,
-created_on TIMESTAMP NOT NULL,
+created_on TIMESTAMP NOT NULL
 );
 
-insert into luis.cloud values(0,'luispf','@pf',now());
-insert into luis.cloud values(0,'carlos','@carlos',now());
-insert into luis.cloud values(0,'maria','@maria',now());
-insert into luis.cloud values(0,'juanperez','@juanperez',now());
+insert into cloud values(1,'luispf','@pf',now());
+insert into cloud values(2,'carlos','@carlos',now());
+insert into cloud values(3,'maria','@maria',now());
+insert into cloud values(4,'juanperez','@juanperez',now());
